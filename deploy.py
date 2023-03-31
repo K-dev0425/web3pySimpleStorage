6a844468c5d3f6515369662e51d156eb472fe36a
K-dev0425 <73441859+K-dev0425@users.noreply.github.com>
2023-03-31 11:58:06 +0800

    get abi, bytecode

--- deploy.py
+++ deploy.py
@@ -1,14 +1,29 @@
-from solcx import compile_standard
+from solcx import compile_standard, install_solc
+import json
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
 
 # Compile Our Standard
 
-
+install_solc("0.8.0")
 compiled_sol = compile_standard(
     {
-        "languages": "Solidity",
-        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}}
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+            }
+        },
     }
-) 
+)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
+
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
