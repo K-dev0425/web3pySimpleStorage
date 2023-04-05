0afa8ffe9fb3d0111985f22ada7d3e6e0728efbe
K-dev0425 <73441859+K-dev0425@users.noreply.github.com>
2023-04-05 22:58:46 +0800

    venv, gitignore

--- deploy.py
+++ deploy.py
@@ -32,3 +32,19 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 # for connection to ganache
 w3 = Web3(Web3.HTTPProvider("http://0.0.0.0:7545"))
 chain_id = 5777
+my_address = "0x1851c1a57b2A69ec695f8943169b29b9537101E9"
+private_key = "0xd61abb0342a5ee594ef07c3421e4b90fe955b3c7bb42d6c6d432678da83d55a8"
+
+# Create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode) 
+# get transaction count
+nonce = w3.eth.get_transaction_count(my_address)
+# submit the transaction that deploys the contract
+transaction = SimpleStorage.constructor().build_transaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
